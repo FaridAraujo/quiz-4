d0ea9093da8b41aa2819993946e2a6789e501576
FaridAraujo <faridabadcr@gmail.com>
2024-08-07 20:52:21 -0600

    Add files via upload

---
+++ quiz3Main/src/quiz3main/Estudiantes.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quiz3main;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Estudiantes {
+
+    private String nombreEstudiante;
+    private int notaEstudiante;
+    private final double promedioNota;
+
+    Estudiantes(String nombreEstudiante, int notaEstudiante, final double promedioNota) {
+        this.nombreEstudiante = nombreEstudiante;
+        this.notaEstudiante = notaEstudiante;
+        this.promedioNota = promedioNota;
+    }
+
+    public String imprimirDatos() {
+        String printEnPantallaEstudiantes = "";
+        String printEnPantallaNotaEstudiantes = "";
+        printEnPantallaEstudiantes = printEnPantallaEstudiantes + ("Porfavor ingrese el nombre del estudiante";
+        
+        return printEnPantallaEstudiantes;
+
+    }
+}
